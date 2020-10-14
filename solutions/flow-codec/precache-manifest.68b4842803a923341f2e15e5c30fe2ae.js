4d1740eb90e4d9a66cf0db73a7507dffb9b2de52
NoName4Me <anich.evas@gmail.com>
2020-10-14 17:23:32 +0000

    Deploying to master from  @ 18f7570c1ffaeca38c220d9c5211a95559b56c9d 🚀

---
+++ solutions/flow-codec/precache-manifest.68b4842803a923341f2e15e5c30fe2ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a0040882609738a181c785c21e37e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7bd88a3414082451ab2",
+    "url": "/static/css/main.12e51390.chunk.css"
+  },
+  {
+    "revision": "57bad8bec077fab193ff",
+    "url": "/static/js/2.5a164282.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5a164282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bd88a3414082451ab2",
+    "url": "/static/js/main.0f27a3e0.chunk.js"
+  },
+  {
+    "revision": "abb9c348889307a668b5",
+    "url": "/static/js/runtime-main.fcc2015a.js"
+  }
+]);

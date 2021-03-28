42ec726aa89e443713b3d88c45dadcd7635955bf
NoName4Me <anich.evas@gmail.com>
2021-03-28 16:53:04 +0000

    Deploying to master from @ NoName4Me/notion-blog@7bd395289704975db95d47b54bd660b4d0a67bce 🚀

---
+++ component---src-pages-404-js-426a21b5b72a5d29c0fa.js
@@ -0,0 +1,2 @@
+(self.webpackChunknotion_blog=self.webpackChunknotion_blog||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var l=n(7294),u=n(5127),o=n(3751);t.default=function(){return l.createElement(u.Z,null,l.createElement(o.Z,{title:"404: Not found"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-426a21b5b72a5d29c0fa.js.map
